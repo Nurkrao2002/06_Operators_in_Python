b8b24c88d49395bf77eaa0d67df2229a9e6a46e1
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+uadaynalabothu@users.noreply.github.com>
2022-09-06 01:38:03 +0530

    Update 01_Arithmetic_Operations.py

--- 01_Arithmetic_Operations.py
+++ 01_Arithmetic_Operations.py
@@ -1,11 +1,11 @@
 """    Arithmetic Operators """
 
-# a=float(input("Enter the value of a:"))
-# b=float(input("Enter the value of b:"))
-# a,b=[int(x) for x in input('Enter the value of a and b: ').split()]
-# print(f"The value of {a} + {b} is {a+b}")
-# print(f"The value of {a} - {b} is {a-b}")
-# print(f"The value of {a} * {b} is {a*b}")
-# print(f"The value of {a} / {b} is {a/b}")
-# print(f"The value of {a} % {b} is {a%b}")
-# print(f"The value of {a} ** {b} is {a**b}")
+a=float(input("Enter the value of a:"))
+b=float(input("Enter the value of b:"))
+a,b=[int(x) for x in input('Enter the value of a and b: ').split()]
+print(f"The value of {a} + {b} is {a+b}")
+print(f"The value of {a} - {b} is {a-b}")
+print(f"The value of {a} * {b} is {a*b}")
+print(f"The value of {a} / {b} is {a/b}")
+print(f"The value of {a} % {b} is {a%b}")
+print(f"The value of {a} ** {b} is {a**b}")
